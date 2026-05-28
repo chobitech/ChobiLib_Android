c431796ed0c63e7b59e861b402c2646d8e649a38
chobitech <chobitech@users.noreply.github.com>
2026-05-28 14:11:41 +0900

    006

--- chobilib/build.gradle.kts
+++ chobilib/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21"
     id("maven-publish")
 }
 
@@ -16,6 +17,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildFeatures {
+        compose = true
     }
 
     /*
