b8fdaac79d5bdbebac0b42059a40696bed807a6e
chobitech <chobitech@users.noreply.github.com>
2026-06-05 06:33:41 +0900

    0.1.9

--- chobilib/build.gradle.kts
+++ chobilib/build.gradle.kts
@@ -46,6 +46,17 @@ publishing {
             }
         }
     }
+
+    repositories {
+        maven {
+            name = "ChobiLib_Android"
+            url = uri("https://maven.pkg.github.com/chobitech/ChobiLib_Android") // リポジトリURL
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 
