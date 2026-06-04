37016d5684f64a37ceaafd68d369c01f0648e078
chobitech <chobitech@users.noreply.github.com>
2026-06-05 06:37:49 +0900

    0.1.10

--- chobilib/build.gradle.kts
+++ chobilib/build.gradle.kts
@@ -40,6 +40,7 @@ publishing {
 
             groupId = "com.github.chobitech"
             artifactId = "chobilib"
+            version = "0.1.10"
 
             afterEvaluate {
                 from(components["release"])
